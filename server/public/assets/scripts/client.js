21dc9a2d35d6aa8c9aff75a003ad99277240d519
Kyra Crowston <kyra.crow00@icloud.com>
2016-01-21 14:40:13 -0600

    second commit

--- server/public/assets/scripts/client.js
+++ server/public/assets/scripts/client.js
@@ -4,8 +4,11 @@
 var app = angular.module('talentAgency', []);
 app.controller('MainController', ['$scope', '$http', function($scope, $http){
     var newTalent = {};
-
-    $scope.skills = ['Cookies', 'Arson', 'Nunchucks', 'Driving Stick'];
+    $scope.usrcooks = false;
+    $scope.usrarson = false;
+    $scope.usrnunchucks = false;
+    $scope.usrstick = false;
+    $scope.skills = {Cookies:false, Arson:false, Nunchuck:false, Stick:false};
     $scope.talent = {
         skills:['talent']
     };
@@ -16,11 +19,83 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http){
         newTalent.phone = $scope.talent.phone;
         newTalent.low_range = $scope.talent.lowrange;
         newTalent.high_range = $scope.talent.highrange;
-        console.log(newTalent);
-        return $http.post('/sendTalent', newTalent).success(function(response){
-            console.log(response);
+        newTalent.cookies = $scope.skills.Cookies;
+        newTalent.arson = $scope.skills.Arson;
+        newTalent.nunchucks = $scope.skills.Nunchucks;
+        newTalent.stick = $scope.skills.Stick;
+
+        return $http.post('/sendTalent', newTalent).success(function(response) {
+            console.log(response[response.length/2]);
+            var serversideObject = response;
+            var skillsArray = [];
+            var bigArray = [];
+
+            //for(var i = 0; i < serversideObject.length; i++){
+            //    if(serversideObject[i].cookies == true){
+            //        $scope.usrcooks = true;
+            //    }
+            //    if(serversideObject[i].arson == true){
+            //        $scope.usrarson = true;
+            //    }
+            //    if(serversideObject[i].nunchucks == true){
+            //        $scope.usrnunchucks = true;
+            //    }
+            //    if(serversideObject[i].stick == true){
+            //        $scope.usrstick = true;
+            //    }
+            //    return $scope.informationHolder = serversideObject;
+            //}
+            $scope.informationHolder = serversideObject;
+
+            //for(var i = 0; i < serversideObject.length; i++){
+            //    skillsArray.push(serversideObject[i].serversideObject[i].cookies);
+            //    skillsArray.push(serversideObject[i].arson);
+            //    skillsArray.push(serversideObject[i].nunchucks);
+            //    skillsArray.push(serversideObject[i].stick);
+            //}
+            //console.log(skillsArray);
         });
+
     };
 
 }]);
+
+///**
+// * Created by kyracrowston on 1/20/16.
+// */
+//var app = angular.module('talentAgency', []);
+//app.controller('MainController', ['$scope', '$http', function($scope, $http){
+//    var newTalent = {};
+//
+//    var data=[];
+//
+//    $scope.skills = {Cookies:false, Arson:false, Nunchuck:false, Stick:false};
+//    $scope.talent = {
+//        skills:['talent']
+//    };
+//
+//    $scope.talentData = data;
+//
+//    $scope.submitTalent = function(){
+//        newTalent.first_name = $scope.talent.firstname;
+//        newTalent.last_name = $scope.talent.lastname;
+//        newTalent.phone = $scope.talent.phone;
+//        newTalent.low_range = $scope.talent.lowrange;
+//        newTalent.high_range = $scope.talent.highrange;
+//        newTalent.cookies = $scope.skills.Cookies;
+//        newTalent.arson = $scope.skills.Arson;
+//        newTalent.nunchucks = $scope.skills.Nunchucks;
+//        newTalent.stick = $scope.skills.Stick;
+//        console.log(newTalent);
+//
+//        return $http.post('/sendTalent', newTalent).success(function(response) {
+//            console.log(response);
+//            data.push(response);
+//        });
+//        console.log(data);
+//    };
+//
+//}]);
+
+
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -15,12 +15,33 @@ router.get("/*", function(request, response){
 });
 
 router.post('/sendTalent', function(request, response){
-   var talentPerson = request.body;
-    console.log(talentPerson);
-    pg.connect(connectionString, function(err, client, done){
-        client.query('INSERT INTO talent(first_name, last_name) values($1, $2)', [talentPerson.first_name, talentPerson.last_name]);
-        client.query('INSERT INTO talent(phone) values()', [talentPerson.phone]);
+    var results = [];
+    var tp = request.body;
+    console.log(tp);
 
+    pg.connect(connectionString, function(err, client, done){
+        client.query('INSERT INTO talent(first_name, last_name, phone, low_range, high_range) values($1, $2, $3, $4, $5)', [tp.first_name, tp.last_name, tp.phone, tp.low_range, tp.high_range]);
+        client.query('INSERT INTO skills(cookies, arson, nunchucks, stick) values($1, $2, $3, $4)', [tp.cookies, tp.arson, tp.nunchucks, tp.stick]);
+
+        //gives us back entire table of talent
+        var query = client.query('SELECT * FROM talent ORDER BY id ASC');
+        var query2 = client.query('SELECT * FROM skills ORDER BY id ASC');
+
+        //takes each row and pushes it to the results array
+        query.on('row', function(row){
+            results.push(row);
+        });
+
+        query2.on('row', function(row){
+            results.push(row);
+            console.log(results);
+        });
+
+        //when function is complete return results array. client.end closes connection w/ database
+        query2.on('end', function(){
+            client.end();
+            return response.json(results);
+        })
     })
 });
 
@@ -43,3 +64,65 @@ router.post('/sendTalent', function(request, response){
 
 
 module.exports = router;
+
+
+///**
+// * Created by kyracrowston on 1/20/16.
+// */
+//var express = require('express');
+//var path = require('path');
+//var pg = require('pg');
+//var router = express.Router();
+//
+//var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/talent_db';
+//
+////routing to index.html, response.send file allows to have more than one destination
+//router.get("/*", function(request, response){
+//    var file = request.params[0] || "/views/index.html";
+//    response.sendFile(path.join(__dirname, "../public/", file));
+//});
+//
+//router.post('/sendTalent', function(request, response){
+//    var results = [];
+//    var tp = request.body;
+//    console.log(tp);
+//
+//    pg.connect(connectionString, function(err, client, done){
+//        client.query('INSERT INTO talent(first_name, last_name, phone, low_range, high_range) values($1, $2, $3, $4, $5)', [tp.first_name, tp.last_name, tp.phone, tp.low_range, tp.high_range]);
+//        client.query('INSERT INTO skills(cookies, arson, nunchucks, stick) values($1, $2, $3, $4)', [tp.cookies, tp.arson, tp.nunchucks, tp.stick]);
+//
+//        //gives us back entire table of talent
+//        var query = client.query('SELECT * FROM talent ORDER BY id ASC');
+//
+//        //takes each row and pushes it to the results array
+//        query.on('row', function(row){
+//            results.push(row);
+//        });
+//
+//        //when function is complete return results array. client.end closes connection w/ database
+//        query.on('end', function(){
+//            client.end();
+//            return response.json(results);
+//        })
+//    })
+//});
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//module.exports = router;
